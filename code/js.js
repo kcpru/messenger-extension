4b2d78b157053ab5acddb98737e2067444a1fd33
Kacper Slusarczyk <kacper12345678909@gmail.com>
2020-05-23 11:44:52 +0200

    Update

--- code/js.js
+++ code/js.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
   $("body").append("<div class='messenger-chat'></div>");
   const element = $(".messenger-chat");
-  element.append(
-    '<iframe src="https://www.messenger.com/" title="Messenger" scrolling="no"></iframe>'
-  );
+  element.append('<iframe class="messenger-iframe" scrolling="no"></iframe>');
+  $(".messenger-iframe").attr("src", "https://www.messenger.com/");
 });
